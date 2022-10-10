9e80d3a6c3a32d366a216b7a1ec9089e2989848d
sandeepmchary <awssandeepchary@gmail.com>
2022-10-11 00:15:42 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect to MongoDB
+mongoose.connect('',
+  // mongo config when working with docker
+  // in the line 12 it is empty, but typically  we have database configuration code
+  { useNewUrlParser: true }
+)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const Item = require('./models/Item');
+
+app.get('/', (req, res) => {
+  console.log("hit home route");
+  
+  Item.find()
+    .then(items => res.render('index', { items }))
+    .catch(err => res.status(404).json({ msg: 'No items found' }));
+});
+
+app.post('/item/add', (req, res) => {
+  
+  const newItem = new Item({
+    
+    name: req.body.name,
+    post: req.body.post
+  });
+
+  newItem.save().then(item => res.redirect('/'));
+});
+
+const port = '' // intemtionally left blank
+
+// which port is the node server listening on?
+
+app.listen(port, () => console.log('I will not run', port));
